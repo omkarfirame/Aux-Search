55a27f7a19c7f27a7207fa0e62237a977fdd42f1
omkarfirame <omkarfirame@gmail.com>
2024-02-29 21:52:08 +0530

    Created a web application using flask

--- app.py
+++ app.py
@@ -1,44 +1,36 @@
 import streamlit as st
 from AuxSearch.components.PDFTextExtractor import PDFConverter
 from AuxSearch.components.TextChunkerEmbedder import TextProcessor
-from langchain_community.vectorstores.faiss import FAISS
 from AuxSearch.constants import *
 from AuxSearch.components.Model import ResponseGenerator
+from flask import Flask, render_template, request
+import openai
 
 pdfconverter = PDFConverter()
 textprocessor = TextProcessor()
 responsegen = ResponseGenerator()
 
+app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-import streamlit as st
+@app.route('/answer', methods=['POST'])
+def answer_question():
+    # Get user inputs
+    question = request.form['question']
+    pdf_file = request.files['pdf_file']
 
-def main():
-    st.title("Question Answering System")
+    # Process PDF file
+    raw_text = pdfconverter.pdf_to_text(pdf_file)
+    text_chunks = textprocessor.get_chunks(raw_text)
+    textprocessor.get_vector_store(text_chunks)
+    rag_chain = responsegen.get_response()
 
-    # File upload
-    uploaded_file = st.file_uploader("Upload a file", type=["pdf"])
-    question = st.text_input("Enter your question")
-
-    if uploaded_file and question:
-        submit_button = st.button("Submit")
-        if submit_button:
-            raw_text = pdfconverter.pdf_to_text(uploaded_file)
-            text_chunks = textprocessor.get_chunks(raw_text)
-            textprocessor.get_vector_store(text_chunks)
+    return render_template('answer.html', question=question, answer=rag_chain.invoke(question))
 
 
-            # Question input
-            
-
-            if st.button("Get Answer"):
-                # Initialize UserInput instance
-                rag_chain = responsegen.get_response()
-                # Display the answer
-                st.subheader("Answer:")
-                st.write(rag_chain.invoke(question))
-        
-if __name__ == "__main__":
-    main()
-
+if __name__ == '__main__':
+    app.run(debug=True)
